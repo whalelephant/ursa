b24397b093b20c5715efaa96849903779b7ba514
bwty <whalelephant@users.noreply.github.com>
2023-04-29 16:25:07 +0800

    tmp: update key size

--- libursa/src/cl/constants.rs
+++ libursa/src/cl/constants.rs
@@ -3,7 +3,7 @@ use bn::{BigNumber, BIGNUMBER_2};
 pub const LARGE_MASTER_SECRET: usize = 256;
 pub const LARGE_E_START: usize = 596;
 pub const LARGE_E_END_RANGE: usize = 119;
-pub const LARGE_PRIME: usize = 1024;
+pub const LARGE_PRIME: usize = 128;
 pub const LARGE_VPRIME: usize = 2128;
 pub const LARGE_VPRIME_PRIME: usize = 2724;
 pub const LARGE_MVECT: usize = 592;
